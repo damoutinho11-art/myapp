d8f1292fccd68ebfa394200f4e48a84761d9b526
damoutinho11-art <da.moutinho11@gmail.com>
2026-06-18 18:58:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "breakthrough-v2";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const CACHE_NAME = "breakthrough-v3";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192_2.png", "./icon-512_2.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
