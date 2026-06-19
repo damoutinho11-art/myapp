77c15ce91467f88e28d9cea797b810c2b6131bee
damoutinho11-art <da.moutinho11@gmail.com>
2026-06-19 23:29:54 +0300

    Add files via upload

---
+++ myapp-main/myapp-main/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "breakthrough-v3";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192_2.png", "./icon-512_2.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(e.request).then((cachedResponse) => {
+        const fetchPromise = fetch(e.request).then((networkResponse) => {
+          if (networkResponse.status === 200) {
+            cache.put(e.request, networkResponse.clone());
+          }
+          return networkResponse;
+        }).catch(() => {});
+        
+        return cachedResponse || fetchPromise;
+      });
+    })
+  );
+});
